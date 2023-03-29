9953fffb378ad43fb5817a4c9bce4a517586653b
scoow <85620078+scoow@users.noreply.github.com>
2023-03-29 18:25:31 +0300

    Сделал таймер

---
+++ RacesPrototype/Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private DateTime _startTime = new();
+    private DateTime _now = new();
+    private TimeSpan _interval;
+    [SerializeField]
+    private Text _timerText;
+
+    private void Start()
+    {
+        ResetTimer();
+    }
+
+    private void ResetTimer()
+    {
+        _startTime = DateTime.Now;
+    }
+
+    private void Update()
+    {
+        CalculateTimeElapsed();
+        ConvertTimeElapsedToString();
+        _timerText.text = ConvertTimeElapsedToString();
+    }
+
+    private string ConvertTimeElapsedToString()
+    {
+        return _interval.Minutes + ":" + _interval.Seconds + ":" + _interval.Milliseconds / 100;
+    }
+
+    private void CalculateTimeElapsed()
+    {
+        _now = DateTime.Now;
+        _interval = _now - _startTime;
+    }
+}
