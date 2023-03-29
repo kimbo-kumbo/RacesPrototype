4ef3bd577303b9cdd8ae813c58101ca5332234c4
kimbo-kumbo <92639132+kimbo-kumbo@users.noreply.github.com>
2023-03-29 23:21:41 +0300

    Добавил заготовку для панели статистики

--- RacesPrototype/Assets/Scripts/FinishTrigger.cs
+++ RacesPrototype/Assets/Scripts/FinishTrigger.cs
@@ -1,15 +1,24 @@
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace RacePrototype
 {
     public class FinishTrigger : MonoBehaviour
     {
+        [SerializeField] private Statistics_Controller statisticsController;
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponent<Body_Marker>() /*|| other.GetComponent<NewPlayer>() */is null)
                 return;
-
+            OnEnableStaticsPanel();
             Debug.Log("Finish");
+        }
+
+
+        private async void OnEnableStaticsPanel()
+        {
+            await Task.Delay(2000);
+            statisticsController.gameObject.SetActive(true);            
         }
     }
 }
